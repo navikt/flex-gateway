1fa64fea5842a9274bac6e7347a6ce8581d388c2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-08-15 12:52:42 +0200

    Fjerner ubrukte APIer (#107)

--- src/main/kotlin/no/nav/helse/flex/gateway/readiness/ReadyController.kt
+++ src/main/kotlin/no/nav/helse/flex/gateway/readiness/ReadyController.kt
@@ -1,53 +1,17 @@
 package no.nav.helse.flex.gateway.readiness
 
-import no.nav.helse.flex.gateway.logger
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.http.*
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.exchange
-import java.net.URI
-import java.time.Duration
 
 const val APPLICATION_READY = "Application is ready!"
-const val APPLICATION_NOT_READY = "Application is NOT ready!"
 
 @RestController
-class SelfTestController(
-    @Value("\${spinnsyn.backend.url}") private val spinnsynBackendUrl: String,
-) {
-
-    var ready = false
-    val log = logger()
-
-    private val restTemplate = RestTemplateBuilder()
-        .setReadTimeout(Duration.ofSeconds(2))
-        .setConnectTimeout(Duration.ofSeconds(1))
-        .build()
-
-    fun spinnsynBackendErOk(): Boolean {
-        val request = RequestEntity<Any>(HttpMethod.GET, URI("$spinnsynBackendUrl/internal/health"))
-        val res: ResponseEntity<String> = restTemplate.exchange(request)
-        return res.statusCode.is2xxSuccessful
-    }
+class SelfTestController {
 
     @GetMapping("/internal/isReady", produces = [MediaType.TEXT_PLAIN_VALUE])
     fun isReady(): ResponseEntity<String> {
-        if (ready) {
-            return ResponseEntity.ok(APPLICATION_READY)
-        }
-        try {
-
-            if (spinnsynBackendErOk()) {
-                log.info("I am ready")
-                ready = true
-                return ResponseEntity.ok(APPLICATION_READY)
-            }
-            throw RuntimeException("Ikke klar")
-        } catch (e: Exception) {
-            log.info("flex-gateway er ikke klar", e)
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(APPLICATION_NOT_READY)
-        }
+        return ResponseEntity.ok(APPLICATION_READY)
     }
 }
